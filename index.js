48d3aecf35877a547e6eac9641322f01957430c9
Moises Lara <laramoises73@gmail.com>
2024-08-24 16:59:54 -0600

    Primer commit al repositorio de bases

---
+++ index.js
@@ -0,0 +1,5 @@
+
+const nombre = "Elmer Moises";
+const apellido = "Gomez Lara"
+
+console.log(`Mi nombre completo es: ${nombre} ${apellido}`);
---
+++ type_basics.js
@@ -0,0 +1,90 @@
+
+
+//VARIABLES
+var nombre = "Elmer Moises Gomez Lara";             //
+let carrera = "Ingenieria en Sistemas";             //
+const universidad = "Universidad Mariano Galvez"    //
+
+alert(`Mi nombre es: ${nombre}, estudio la ${carrera}, en la ${universidad}`);
+
+
+//REGAS PARA NOMBRAR VARIABLES
+
+
+//TIPOS DE DATOS
+
+/*PRIMITIVOS*/
+/*
+-number
+-string
+-boolean
+-undefined
+-null
+*/
+
+/*DE REFERENCIA*/
+/*
+-array
+-object
+-functions
+-clases
+*/
+
+
+//TIPOS PRIMITIVOS
+let numero  = 1;            //NUMBER
+let texto   = "Hola Mundo"  //STRING
+let verdadero = true;       //BOOLEAN
+let falso = false;          //BOOLEAN
+let noDefinido;             //UNDEFINED
+let undef = undefined;      //UNDEFINED
+let nulo = null;            //NULL
+
+
+//CONSTANTES
+let nombre1 = "Hola Mundo";
+nombre1 = "Chancito Feliz";
+console.log(nombre1);
+
+//TIPO DINAMICO
+let numero1 = 1;
+let texto1 = "Hola Mundo";
+let verdadero1 = true;
+let undef1;
+let nula = null;
+
+console.log(typeof numero1);
+console.log(typeof texto1);
+console.log(typeof verdadero1);
+console.log(typeof undef1);
+console.log(typeof nula);
+
+
+//COMENTARIOS
+
+//Comentario de una lineas
+
+/** 
+*Comentario multilinea
+**/
+
+
+//OBJETOS
+let nombreAnime = "Demon Slayer";
+let nombrePersonaje = "Tanjiro"
+let edadPersonaje = 16;
+
+let personajeObject = {
+    nombre: "Tanjiro",
+    anime: "Demon Slayer",
+    edad: 16,
+}
+
+//MANDAR EN PATALLA UN VALOR DE UN OBJETO
+console.log(personajeObject.anime);
+
+
+//FUNCIONES
+
+
+//ARGUMENTOS Y PARAMETROS
